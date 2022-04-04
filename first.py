18557a0394387e866e4ce1fdd5a9c6e4c8f84ee7
chrilona <christabelalona@gmail.com>
2022-04-04 07:47:22 +0300

    small game

---
+++ first.py
@@ -0,0 +1,6 @@
+#a simple program about a celebrity
+print("A celebrity")
+celebrity_name= input("celebrity_name")
+favourite_color= input("favorite_ color")
+favourite_food= input("favourite_food")
+print(celebrity_name + favourite_color + favourite_food)
